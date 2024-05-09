bf4eaadef926c56999818efefc59a1da70ce36d8
Wanchana <s.wanchana41@gmail.com>
2024-05-09 12:56:18 +0700

    feat: add variables & constant variables, reassign variables value[ex.1-2]

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,27 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+console.log(firstName);
+console.log(lastName);
+console.log(address);
+
+const restaurantName = "Chef's Table by Chef James";
+const netPrice = 15000;
+const totalActiveRestaurant = 1500;
+
+console.log(restaurantName);
+console.log(netPrice);
+console.log(totalActiveRestaurant);
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+netPrice = 5000;
+totalActiveRestaurant = 2500;
+
+console.log(address);
+console.log(netPrice);
+console.log(totalActiveRestaurant);
