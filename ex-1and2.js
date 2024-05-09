82bb72dc0d683e25b4f198123df395437c3f2b62
Kittanut Thammapibaljit <tkittanut@gmail.com>
2024-05-09 12:35:09 +0700

    finished ex 1-4.

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,16 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const totalBillExpenses = 15000;
+const totalOpeningRestaurant = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalBillExpenses = 5000; //TypeError
+totalOpeningRestaurant = 2500; //TypeError
+
